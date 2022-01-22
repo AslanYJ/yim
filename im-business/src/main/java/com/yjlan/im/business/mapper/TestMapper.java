79e4b5c042be748d5bb03ddf5d1e6c79ed5461f2
lanyangjia <121601585@qq.com>
2022-01-22 23:57:54 +0800

    feat： 提交生成Mybatis的模板文件

---
+++ im-business/src/main/java/com/yjlan/im/business/mapper/TestMapper.java
@@ -0,0 +1,21 @@
+package com.yjlan.im.business.mapper;
+
+import com.yjlan.im.business.entity.Test;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TestMapper {
+
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    Test selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
+}
