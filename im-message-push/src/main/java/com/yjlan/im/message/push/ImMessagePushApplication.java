b42193d279c39b2418307a1cf212906a582703fe
lanyangjia <121601585@qq.com>
2022-01-25 23:42:53 +0800

    feat：新增一个模块im-message-push

---
+++ im-message-push/src/main/java/com/yjlan/im/message/push/ImMessagePushApplication.java
@@ -0,0 +1,17 @@
+package com.yjlan.im.message.push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 消息推送服务
+ * @author yjlan
+ */
+@SpringBootApplication
+public class ImMessagePushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImMessagePushApplication.class);
+
+    }
+}
